4e903e4b837e5184bb28de8b24dc356e2993103a
Abdelaziz Ouhammou <abdelaziz.ouhammou@gmail.com>
2023-08-06 02:25:24 +0300

    added final version of main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net"
+	"os"
+)
+
+var blockedIPs = map[string]struct{}{}
+
+func main() {
+	badIPChannel := make(chan net.IP, 10)
+	errChannel := make(chan error)
+	journal := NewJournal(badIPChannel, errChannel, 3)
+	iptablesBlocker, err := NewIPtablesBlocker(badIPChannel, errChannel)
+	if err != nil {
+		fatalLogger.Println(err)
+		os.Exit(1)
+	}
+
+	go journal.Run()
+	go iptablesBlocker.startBlockingIPs()
+
+	infoLogger.Println("brutedef running...")
+	// read errors from errChannel and exit when an err is received
+	err = <-errChannel
+	if err != nil {
+		fatalLogger.Println(err)
+		os.Exit(1)
+	}
+}
